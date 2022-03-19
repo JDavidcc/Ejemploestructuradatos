3567758652cd7b0c8c91146b2b26b79fe5515570
JDavidcc <jesus_d_cc@hotmail.com>
2022-03-19 12:19:02 -0700

    Agregar archivos de proyecto.

---
+++ Ejemploestructuradatos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejemploestructuradatos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
